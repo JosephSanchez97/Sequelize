eb51d4c1482fe229bd6d36669fb1b358642278a4
Joseph Sanchez <sanchezjoseph378@gmail.com>
2021-03-30 16:58:45 -0400

    ADD: INDEX SCRIPT JS, STYLE CSS

--- public/script.js
+++ public/script.js
@@ -1,5 +1,18 @@
-async function dininghalls() {
-  const request = await fetch("/api/dining");
-  const data = await request.json();
+function windowActions() {
+  diningHalls()
 }
-window.onload = dininghalls;
+
+async function diningHalls() {
+  const table = document.querySelector('.table-body')
+  const request = await fetch('/api/dining')
+  const halls = await request.json()
+  console.log(halls)
+  halls.data.forEach(place => {
+    table.innerHTML += `<tr>
+    <td>${place.hall_id}</td>
+    <td>${place.hall_name}</td>
+    <td>${place.hall_address}</td>
+    </tr>`
+  })
+}
+window.onload = windowActions();
